ae2b031afd2f544bb2e4739c58580e58f40b1990
zly321 <zhangliye0721@163.com>
2025-08-03 09:53:54 +0800

    条件构造器-UpdateWrapper&LambdaQueryWrapper

--- mp-demo/src/test/java/com/itheima/mp/mapper/WrapperTest.java
+++ mp-demo/src/test/java/com/itheima/mp/mapper/WrapperTest.java
@@ -1,6 +1,8 @@
 package com.itheima.mp.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.itheima.mp.domain.po.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -49,4 +51,38 @@ public class WrapperTest {
         userMapper.update(user, queryWrapper);
 
     }
+
+    /**
+     * 更新id为1,2,4的用户的余额，扣200
+     *
+     * UPDATE user SET balance = balance - 1 where id in(1,2,4)
+     */
+    @Test
+    public void testUpdateWrapper() {
+
+        UpdateWrapper<User> updateWrapper = new UpdateWrapper<>();
+
+        updateWrapper.setSql("balance = balance - 200");
+        //条件
+        updateWrapper.in("id",1, 2, 4);
+
+        userMapper.update(null,updateWrapper);
+    }
+
+    /**
+     * 查询出名字中带o的，存款大于等于1000元的人（id,username,info,balance）
+     */
+    @Test
+    public void testLambdaQueryWrapper() {
+        //1、构造查询条件；构造 where username like 'o' and balance >= 1000
+        LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.
+                select(User::getId, User::getUsername, User::getInfo, User::getBalance)
+                .like(User::getUsername, "o")
+                .ge(User::getBalance, 1000);
+        //2、查询
+        List<User> list = userMapper.selectList(queryWrapper);
+        list.forEach(System.out::println);
+    }
+
 }
