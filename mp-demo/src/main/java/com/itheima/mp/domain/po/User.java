ef6d8d89a923d3a354aad1b4c3d884f438db8262
zly321 <zhangliye0721@163.com>
2025-08-03 20:09:11 +0800

    Json类型处理器

--- mp-demo/src/main/java/com/itheima/mp/domain/po/User.java
+++ mp-demo/src/main/java/com/itheima/mp/domain/po/User.java
@@ -1,11 +1,15 @@
 package com.itheima.mp.domain.po;
 
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.extension.handlers.JacksonTypeHandler;
 import com.itheima.mp.enums.UserStatus;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
+@TableName(value = "user", autoResultMap = true)
 public class User {
 
     /**
@@ -31,7 +35,8 @@ public class User {
     /**
      * 详细信息
      */
-    private String info;
+    @TableField(typeHandler = JacksonTypeHandler.class)
+    private UserInfo info;
 
     /**
      * 使用状态（1正常 2冻结）
---
+++ mp-demo/src/main/java/com/itheima/mp/domain/po/UserInfo.java
@@ -0,0 +1,14 @@
+package com.itheima.mp.domain.po;
+
+import lombok.Data;
+
+@Data
+public class UserInfo {
+
+    //简介
+    private String intro;
+    //年龄
+    private Integer age;
+    //性别
+    private String gender;
+}
