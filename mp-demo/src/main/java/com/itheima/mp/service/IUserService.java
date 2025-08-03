62e3b9a9a97a867b19c747348562460125d9a056
zly321 <zhangliye0721@163.com>
2025-08-03 10:39:25 +0800

    Service接口-接口与类编写

---
+++ mp-demo/src/main/java/com/itheima/mp/service/IUserService.java
@@ -0,0 +1,9 @@
+package com.itheima.mp.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.itheima.mp.domain.po.User;
+
+public interface IUserService extends IService<User> {
+
+
+}
